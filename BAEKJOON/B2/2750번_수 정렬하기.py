c8b434ff841ad378e4df3c515f6a0659791e5ff0
newkimjiwon <newkimjiwon@naver.com>
2025-07-28 01:59:10 +0900

    B2

---
+++ BAEKJOON/B2/2750번_수 정렬하기.py
@@ -0,0 +1,16 @@
+def main():
+    n = int(input())
+
+    result = []
+
+    for _ in range(n):
+        number = int(input())
+        result.append(number)
+
+    result.sort()
+
+    for i in result:
+        print(i)
+
+
+main()
