ad3dbd295f07281ff5d9f6a4e1cb65067d1c0733
newkimjiwon <newkimjiwon@naver.com>
2025-09-14 00:18:22 +0900

    S5

---
+++ BAEKJOON/S5/9625번_BABBA.py
@@ -0,0 +1,13 @@
+if __name__=="__main__":
+    k = int(input())
+
+    dp = [[0, 0] for _ in range(46)]
+
+    dp[0] =[1, 0]
+    dp[1] =[0, 1]
+    dp[2] =[1, 1]
+
+    for i in range(3, k + 1):
+        dp[i] = [dp[i - 1][0] + dp[i - 2][0], dp[i - 1][1] + dp[i - 2][1]]
+
+    print(dp[k][0], dp[k][1])
